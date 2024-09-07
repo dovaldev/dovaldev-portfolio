764b6d779c702d6cca5fb271aa43382bc6169004
J.A. Doval <73448899+dovaldev@users.noreply.github.com>
2024-09-07 12:37:29 +0200

    first commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,17 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+
+import tailwind from "@astrojs/tailwind";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind(), mdx()],
+  i18n: {
+    locales: ["en", "es"],
+    defaultLocale: "es",
+    routing: {
+      prefixDefaultLocale: true,
+    },
+  },
+});
---
+++ src/js/global.js
@@ -0,0 +1,2 @@
+export const NANE = "Juan A. Doval"
+export const FILE_NAME = "juan-a-doval-nogueira-curriculum-vitae"
